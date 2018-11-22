feeee802face1359a5f40fc49328544f8fd8a24b
ahmuwk14 <41890523+ahmuwk14@users.noreply.github.com>
2018-11-23 00:59:07 +0200

    Add files via upload

---
+++ sefrwahed_emails.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+
+gmail_user = "sefrwahed01@gmail.com"
+gmail_pwd = "SefrWa7edCodegeist18"
+
+def mail(to, text, subject="test subject"):
+   
+   
+
+
+   msg = MIMEMultipart()
+
+   msg['From'] = gmail_user
+   msg['To'] = to
+   msg['Subject'] = subject
+
+   msg.attach(MIMEText(text))
+
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(gmail_user, gmail_pwd)
+   mailServer.sendmail(gmail_user, to, msg.as_string())
+   mailServer.close()
+
+
+
+
+ 
+     f = open("try1.txt","r")
+
+   for line in f:
+          
+     mail(line, "bla bla bal bal bla", subject)
